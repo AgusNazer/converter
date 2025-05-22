cecd91236f27dde9bd4d2db2cd6e270c82604f68
agusNazer <agustin.nazer@hotmail.com>
2025-05-22 20:16:20 -0300

    endpoints user

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,6 +1,8 @@
-import { Controller, Get, Post, Param, Body } from "@nestjs/common";
+import { Controller, Get, Post, Delete, Param, Body } from "@nestjs/common";
 import { UsersService } from "./users.service";
 import { User } from "src/model/user.entity";
+import { CreateUserDto } from "./create-user.dto";
+import { log } from "console";
 
 @Controller('/users')
 export class UsersController{
@@ -17,8 +19,29 @@ export class UsersController{
     }
 
     @Post()
-  createUser(@Body() data: Partial<User>): Promise<User> {
-    return this.usersService.createUser(data);
+  createUser(@Body() dto: CreateUserDto){
+    console.log(dto, ":dto");
+      if (!dto) {
+    return 'No se recibió body';
   }
+    
+    return this.usersService.createUser(dto);
+  }
+//cerar suuarios ismulateaneamente
+@Post('bulk')
+createUsersBulk(@Body() dtos: CreateUserDto[]) {
+  console.log(dtos, ':dto');
+  if (!dtos || !Array.isArray(dtos)) {
+    return 'Se esperaba un array de usuarios';
+  }
+  return this.usersService.createUsersBulk(dtos);
+}
+
+
+@Delete(':id')
+async deleteUserById(@Param('id') id: string): Promise<{ message: string }> {
+  await this.usersService.deleteUserById(+id);
+  return { message: `Usuario con id ${id} borrado satisfactoriamente` };
+}
 
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -2,7 +2,8 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { User } from 'src/model/user.entity';
-
+import { CreateUserDto } from './create-user.dto';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class UsersService {
@@ -19,9 +20,38 @@ export class UsersService {
     return this.usersRepository.findOneBy({ id }); // busca usuario por id
   }
 
-  async createUser(data: Partial<User>): Promise<User> {
-    const newUser = this.usersRepository.create(data);
-    return this.usersRepository.save(newUser);
+async createUser(dto: CreateUserDto): Promise<User> {
+    const user = new User();
+    user.name = dto.name;
+    user.email = dto.email;
+    user.passwordHash = await bcrypt.hash(dto.password, 10);
+
+    return this.usersRepository.save(user);
   }
+
+  async deleteUserById(id: number): Promise<void>{
+    await this.usersRepository.delete(id);
+  }
+
+  //crear usuaarios simlataneamente
+async createUsersBulk(dtos: CreateUserDto[]): Promise<User[]> {
+  const users = await Promise.all(
+    dtos.map(async (dto) => {
+      if (!dto.password) {
+        throw new Error('Password missing in one of the users');
+      }
+      const user = new User();
+      user.name = dto.name;
+      user.email = dto.email;
+      user.passwordHash = await bcrypt.hash(dto.password, 10);
+      return user;
+    }),
+  );
+
+  return this.usersRepository.save(users);
+}
+
+
+
   
 }
